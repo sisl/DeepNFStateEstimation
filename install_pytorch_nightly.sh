3c45d8e46a5fe86f24b41550e6c544a98502e935
Marc Schlichting <mschlichting@DNa82921b.SUNet>
2022-10-25 23:19:50 -0700

    changed nflows installation to our fork

--- install_pytorch_nightly.sh
+++ install_pytorch_nightly.sh
@@ -1,3 +1,3 @@
 pip3 install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
 
-pip3 install nflows
+pip3 install git+https://github.com/nflows.git#egg=nflows
