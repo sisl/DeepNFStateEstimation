8081ee07279e8f389dab609edd93a9ce174ab9c4
Marc Schlichting <50354240+MarcSchlichting@users.noreply.github.com>
2022-10-26 09:53:23 -0700

    Update install_pytorch_nightly.sh

--- install_pytorch_nightly.sh
+++ install_pytorch_nightly.sh
@@ -1,3 +1,3 @@
 pip3 install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
 
-pip3 install git+https://github.com/nflows.git#egg=nflows
+pip3 install git+https://github.com/marcschlichting/nflows.git#egg=nflows
