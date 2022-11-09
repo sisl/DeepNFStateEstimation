b5dc072cfbf87caea52222db0493e46213ad5217
Liam Kruse <lamk98@gmail.com>
2022-11-08 18:17:42 -0800

    Masked Affine Autoregressive Flow successfully conditioned on discrete bicycle dataset

--- nflows_example.py
+++ nflows_example.py
@@ -1,20 +1,22 @@
 #%%
-import matplotlib.pyplot as plt
-import sklearn.datasets as datasets
-
+# Utility imports
 import h5py
+import matplotlib.pyplot as plt
 import numpy as np
 
+# PyTorch imports
 import torch
 from torch import nn
 from torch import optim
 
-from nflows.flows.base import Flow
+# nflows imports
 from nflows.distributions.normal import ConditionalDiagonalNormal
-from nflows.transforms.base import CompositeTransform
+from nflows.flows.base import Flow
+from nflows.nn.nets import MLP
 from nflows.transforms.autoregressive import MaskedAffineAutoregressiveTransform
-from nflows.transforms.permutations import ReversePermutation
-from nflows.transforms.permutations import RandomPermutation
+from nflows.transforms.base import CompositeTransform
+from nflows.transforms.permutations import RandomPermutation, ReversePermutation
+from nflows.transforms import LULinear
 
 # %%
 with h5py.File("bicycle_dataset_discrete.h5", 'r') as f:
@@ -22,25 +24,26 @@ with h5py.File("bicycle_dataset_discrete.h5", 'r') as f:
 
 mask = time>2
 position = torch.Tensor(position).T
-time = torch.Tensor(time)
+time = torch.IntTensor(time)
 
 # Test to remove deterministic start
 position = position[mask]
 time = time[mask]
 
-plt.scatter(position[:, 0], position[:, 1], s = 1, c=time);
+plt.scatter(position[0:2000, 0], position[0:2000, 1], s = 1, c=time[0:2000]);
 #%%
-num_layers = 5
-#base_dist = ConditionalDiagonalNormal(shape=[2], 
-#                                      context_encoder=nn.Sequential(nn.Linear(1,16), nn.Tanh(), nn.Linear(16,4)))
+num_layers = 10
 base_dist = ConditionalDiagonalNormal(shape=[2], 
-                                      context_encoder=nn.Linear(1,4))
+                                      context_encoder=MLP([1], [4], hidden_sizes=[32,32]))
 transform_list = []
 for _ in range(num_layers):
     transform_list.append(RandomPermutation(features=2))
-    transform_list.append(MaskedAffineAutoregressiveTransform(features=2, 
-                                                          hidden_features=4, 
-                                                          context_features=1))
+    transform_list.append(LULinear(2, identity_init=True)),
+    transform_list.append(MaskedAffineAutoregressiveTransform(
+        features=2, 
+        hidden_features=4, 
+        context_features=1,
+        use_batch_norm=False))
 transform = CompositeTransform(transform_list)
 
 flow = Flow(transform, base_dist)
@@ -48,90 +51,37 @@ optimizer = optim.Adam(flow.parameters())
 
 
 #%%
-num_iter = 5000
+num_iter = 2500
 loss_arr = []
 for i in range(num_iter):
     indices = torch.randperm(len(position))[:1024]
     x = torch.tensor(position[indices], dtype=torch.float32)
+    x = (x-torch.mean(x,dim=0))/torch.std(x,dim=0)
     y = torch.tensor(time[indices], dtype=torch.float32).reshape(-1, 1)
 
     optimizer.zero_grad()
     loss = -flow.log_prob(inputs=x, context=y).mean()
-    loss_arr.append(loss)
+    loss_arr.append(loss.detach())
     loss.backward()
     optimizer.step()
     
-    if (i + 1) % 500 == 0:
-        xline = torch.linspace(0.0, 80.0, 100)
-        yline = torch.linspace(0.0, 80.0, 100)
+    if (i < 10) or (i + 1) % 500 == 0:
+        xline = torch.linspace(-4.0, 4.0, 200)
+        yline = torch.linspace(-4.0, 4.0, 200)
         xgrid, ygrid = torch.meshgrid(xline, yline)
         xyinput = torch.cat([xgrid.reshape(-1, 1), ygrid.reshape(-1, 1)], dim=1)
-
+        
         with torch.no_grad():
-            zgrid = flow.log_prob(xyinput, torch.full([10000, 1], 14.0)).exp().reshape(100, 100)
+            zgrid = flow.log_prob(xyinput, torch.full([40000, 1], 14.0)).exp().reshape(200, 200)
 
-        plt.contourf(xgrid.numpy(), ygrid.numpy(), zgrid.numpy(), levels = 10)
+        new_xline = (xline*torch.std(position[indices][:,0],dim=0))+torch.mean(position[indices][:,0],dim=0)
+        new_yline = (yline*torch.std(position[indices][:,1],dim=0))+torch.mean(position[indices][:,1],dim=0)
+        new_xgrid, new_ygrid = torch.meshgrid(new_xline, new_yline)
+
+        plt.contourf(new_xgrid.numpy(), new_ygrid.numpy(), zgrid.numpy(), levels = 100)
+        x = (x*torch.std(position[indices],dim=0))+torch.mean(position[indices],dim=0)
+        plt.scatter(x[:,0], x[:,1], color = "k", s=1)
         plt.title('iteration {}'.format(i + 1))
         plt.show()
-# %%
 
-
-import matplotlib.pyplot as plt
-import sklearn.datasets as datasets
-
-import torch
-from torch import nn
-from torch import optim
-
-from nflows.flows.base import Flow
-from nflows.distributions.normal import ConditionalDiagonalNormal
-from nflows.transforms.base import CompositeTransform
-from nflows.transforms.autoregressive import MaskedAffineAutoregressiveTransform
-from nflows.transforms.permutations import ReversePermutation
-from nflows.nn.nets import ResidualNet
-# %%
-x, y = datasets.make_moons(128, noise=.1)
-plt.scatter(x[:, 0], x[:, 1], c=y);
-# %%
-num_layers = 5
-base_dist = ConditionalDiagonalNormal(shape=[2], 
-                                      context_encoder=nn.Linear(1, 4))
-
-transforms = []
-for _ in range(num_layers):
-    transforms.append(ReversePermutation(features=2))
-    transforms.append(MaskedAffineAutoregressiveTransform(features=2, 
-                                                          hidden_features=4, 
-                                                          context_features=1))
-transform = CompositeTransform(transforms)
-
-flow = Flow(transform, base_dist)
-optimizer = optim.Adam(flow.parameters())
-# %%
-
-num_iter = 5000
-for i in range(num_iter):
-    x, y = datasets.make_moons(128, noise=.1)
-    x = torch.tensor(x, dtype=torch.float32)
-    y = torch.tensor(y, dtype=torch.float32).reshape(-1, 1)
-    optimizer.zero_grad()
-    loss = -flow.log_prob(inputs=x, context=y).mean()
-    loss.backward()
-    optimizer.step()
-    
-    if (i + 1) % 500 == 0:
-        fig, ax = plt.subplots(1, 2)
-        xline = torch.linspace(-1.5, 2.5, 100)
-        yline = torch.linspace(-.75, 1.25, 100)
-        xgrid, ygrid = torch.meshgrid(xline, yline)
-        xyinput = torch.cat([xgrid.reshape(-1, 1), ygrid.reshape(-1, 1)], dim=1)
-
-        with torch.no_grad():
-            zgrid0 = flow.log_prob(xyinput, torch.zeros(10000, 1)).exp().reshape(100, 100)
-            zgrid1 = flow.log_prob(xyinput, torch.ones(10000, 1)).exp().reshape(100, 100)
-
-        ax[0].contourf(xgrid.numpy(), ygrid.numpy(), zgrid0.numpy())
-        ax[1].contourf(xgrid.numpy(), ygrid.numpy(), zgrid1.numpy())
-        plt.title('iteration {}'.format(i + 1))
-        plt.show()
 # %%
